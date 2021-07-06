81abd103932d5252587f57f74f59ccef2aa17799
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-06 16:13:29 -0400

    balanceOf

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -6,6 +6,11 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    // Track Balances
+    mapping(address => uint256) public balanceOf;
+
+    // Send Tokens
+
     constructor() public {
         totalSupply = 1000000 * (10**decimals);
     }
