6c4c0955234eb8f22f0cbd0837191bb3780d9078
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-07 17:47:53 -0400

    Check exchange token balance added 100 tokens to user1

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,4 +1,17 @@
 pragma solidity ^0.5.0;
+import './Token.sol';
+
+// TODO:
+// [x] Set the fee account
+// [ ] Deposit Ether
+// [ ] Withdraw Ether
+// [ ] Deposit tokens
+// [ ] Withdraw tokens
+// [ ] Check balances
+// [ ] Make order
+// [ ] Cancel order
+// [ ] Fill order
+// [ ] Charge fees
 
 contract Exchange {
     // Variables
@@ -9,15 +22,12 @@ contract Exchange {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
+
+    function depositToken(address _token, uint256 _amount) public {
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        // Which token?
+        // How much?
+        // Manage deposit - update balance
+        // Emit event
+    }
 }
-// TODO:
-// [ ] Set the fee account
-// [ ] Deposit Ether
-// [ ] Withdraw Ether
-// [ ] Deposit tokens
-// [ ] Withdraw tokens
-// [ ] Check balances
-// [ ] Make order
-// [ ] Cancel order
-// [ ] Fill order
-// [ ] Charge fees
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,15 +1,21 @@
 import { tokens, EVM_REVERT } from './helpers';
 
 // Test Smart Contract with Truffle
+const Token = artifacts.require('./Token');
 const Exchange = artifacts.require('./Exchange');
 
 require('chai').use(require('chai-as-promised')).should();
 
-contract('Exchange', ([deployer, feeAccount]) => {
+contract('Exchange', ([deployer, feeAccount, user1]) => {
+	let token;
 	let exchange;
 	const feePercent = 10;
 	beforeEach(async () => {
-		// Fetch token from blockchain
+		// Deploy token
+		token = await Token.new();
+		// Transfer tokens to user 1
+		token.transfer(user1, tokens(100), { from: deployer });
+		// Deploy Exchange
 		exchange = await Exchange.new(feeAccount, feePercent);
 	});
 
@@ -21,6 +27,27 @@ contract('Exchange', ([deployer, feeAccount]) => {
 		it('tracks the fee precent', async () => {
 			const result = await exchange.feePercent();
 			result.toString().should.equal(feePercent.toString());
+		});
+	});
+	describe('depositing tokens', () => {
+		let result;
+		let amount;
+
+		beforeEach(async () => {
+			amount = tokens(10);
+			await token.approve(exchange.address, amount, { from: user1 });
+			const result = await exchange.depositToken(token.address, amount, {
+				from: user1,
+			});
+		});
+		describe('success', () => {
+			it('tracks the token deposit', async () => {
+				// Check exchange token balance
+				let balance;
+				balance = await token.balanceOf(exchange.address);
+				balance.toString().should.equal(amount.toString());
+			});
+			describe('failure', () => {});
 		});
 	});
 });
