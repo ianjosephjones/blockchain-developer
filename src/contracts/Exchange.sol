7b0f57adbddfb6e71e839b03ae223c9aeeb196b9
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-07 16:44:05 -0400

    Init Exchange Sol file

---
+++ src/contracts/Exchange.sol
@@ -0,0 +1,12 @@
+// TODO:
+// [ ] Set the fee account
+// [ ] Deposit Ether
+// [ ] Withdraw Ether
+// [ ] Deposit tokens
+// [ ] Withdraw tokens
+// [ ] Check balances
+// [ ] Make order
+// [ ] Cancel order
+// [ ] Fill order
+// [ ] Charge fees
+
