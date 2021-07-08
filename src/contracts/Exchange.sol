b1a0443cd7d6f85099389737f57f203a2d8fec88
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-08 11:00:30 -0400

    Deposit func

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -22,15 +22,18 @@ contract Exchange {
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
 
+    // Events
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
+
     constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
 
     function depositToken(address _token, uint256 _amount) public {
+        
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
         tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
-        // Manage deposit - update balance
-        // Emit event
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -36,7 +36,7 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
 		beforeEach(async () => {
 			amount = tokens(10);
 			await token.approve(exchange.address, amount, { from: user1 });
-			const result = await exchange.depositToken(token.address, amount, {
+			result = await exchange.depositToken(token.address, amount, {
 				from: user1,
 			});
 		});
@@ -46,11 +46,23 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
 				let balance;
 				balance = await token.balanceOf(exchange.address);
 				balance.toString().should.equal(amount.toString());
-                // Check tokens on exchange 
+				// Check tokens on exchange
 				balance = await exchange.tokens(token.address, user1);
 				balance.toString().should.equal(amount.toString());
 			});
-			describe('failure', () => {});
+			it('emits a Deposit event', async () => {
+				const log = result.logs[0];
+				log.event.should.eq('Deposit');
+				const event = log.args;
+				event.token.should.equal(token.address, 'token address is correct');
+				event.user.should.equal(user1, 'user address is correct');
+				event.amount.toString().should.equal(tokens.toString(10), 'amount is correct');
+				event.balance.toString().should.equal(tokens.toString(10), 'balance is correct');
+			});
+
+			describe('failure', () => {
+				
+			});
 		});
 	});
 });
