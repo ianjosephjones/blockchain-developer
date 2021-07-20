0e21f20ae8fc49d55d647433a7b7ebfbeeb607b7
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-20 14:50:41 -0400

    Give tokens to account[1]

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -13,6 +13,14 @@ module.exports = async function (callback) {
 		const exchange = await Exchange.deployed();
 		console.log('Token Fetched', exchange.address);
 
+		// Give tokens to account[1]
+		const sender = accounts[0];
+		const receiver = accounts[1];
+		let amount = web3.unils.toWei('10000', 'ether'); // 10,000 tokens
+
+		await token.transfer(receiver, amount, { from: sender });
+		console.log(`Transferred ${amount} tokens from ${sender} to ${receiver}`);
+
         
 
 		console.log('script running');
