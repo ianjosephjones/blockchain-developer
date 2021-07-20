2227b2757fdc1a74a4c96aff4e61b9c7754aed59
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-20 15:10:16 -0400

    Seed Filled Orders

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -61,6 +61,64 @@ module.exports = async function (callback) {
 		orderId = result.logs[0].args.id;
 		await exchange.cancelOrder(orderId, { from: user1 });
 		console.log(`Cancelled order from ${user1}`);
+
+		/////////////////////////////////////////////////////////////
+		// Seed Filled Orders
+		//
+
+		// User 1 makes order
+		result = await exchange.makeOrder(
+			token.address,
+			tokens(100),
+			ETHER_ADDRESS,
+			ether(0.1),
+			{ from: user1 }
+		);
+		console.log(`Made order from ${user1}`);
+
+		// User 2 fills order
+		orderId = result.logs[0].args.id;
+		await exchange.fillOrder(orderId, { from: user2 });
+		console.log(`Filled order from ${user1}`);
+
+		// Wait 1 second
+		await wait(1);
+
+		// User 1 makes another order
+		result = await exchange.makeOrder(
+			token.address,
+			tokens(50),
+			ETHER_ADDRESS,
+			ether(0.01),
+			{ from: user1 }
+		);
+		console.log(`Made order from ${user1}`);
+
+		// User 2 fills another order
+		orderId = result.logs[0].args.id;
+		await exchange.fillOrder(orderId, { from: user2 });
+		console.log(`Filled order from ${user1}`);
+
+		// Wait 1 second
+		await wait(1);
+
+		// User 1 makes final order
+		result = await exchange.makeOrder(
+			token.address,
+			tokens(200),
+			ETHER_ADDRESS,
+			ether(0.15),
+			{ from: user1 }
+		);
+		console.log(`Made order from ${user1}`);
+
+		// User 2 fills final order
+		orderId = result.logs[0].args.id;
+		await exchange.fillOrder(orderId, { from: user2 });
+		console.log(`Filled order from ${user1}`);
+
+		// Wait 1 second
+		await wait(1);
 	} catch (error) {
 		console.log(error);
 	}
