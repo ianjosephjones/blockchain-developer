6cf6b3f69ae650c5a3f69a6aa479593372fae185
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-20 14:46:51 -0400

    fetch the deployed token & fetch the deployed exchange

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -6,6 +6,15 @@ module.exports = async function (callback) {
 		// fetch accounts from wallet - these are unlocked
 		const accounts = await web3.eth.getAccounts();
 
+		// fetch the deployed token
+		const token = await Token.deployed();
+		console.log('Token Fetched', token.address);
+		// fetch the deployed exchange
+		const exchange = await Exchange.deployed();
+		console.log('Token Fetched', exchange.address);
+
+        
+
 		console.log('script running');
 	} catch (error) {
 		console.log(error);
